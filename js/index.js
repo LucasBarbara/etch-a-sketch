cbe967433d6ef8d354fce248b501e47366d5a431
Barbara <barbara.f.a.lucas@gmail.com>
2019-03-14 21:18:57 +0000

    Clean up the JavaScript code and add some comments

--- js/index.js
+++ js/index.js
@@ -1,14 +1,16 @@
 let gridSize = 16;
 
 const container = document.querySelector('.container');
-
 const button = document.querySelector('#clearGrid');
-
 const btn = document.querySelector('#randomColor');
 
 drawGrid(gridSize);
 
+// Function that draws a grid with the requested size
+
 function drawGrid(gridSize) {
+
+  container.innerHTML = '';
   for (let j = 0; j < gridSize; j++) {
     for (let i = 0; i < gridSize; i++) {
       let content = document.createElement('div');
@@ -16,26 +18,27 @@ function drawGrid(gridSize) {
       container.appendChild(content);
     }
   }
-    paint();
+  paint();
 }
+
+// Prompt the user for the new grid size
 
 function newGrid() {
 
   let newGridSize = +prompt('How many squares by side should the new grid be?');
-
-  while(container.firstChild) {
-    container.removeChild(container.firstChild);
-  }
   drawGrid(newGridSize);
   gridTemplate(newGridSize);
 }
 
+// Paints each square on hover with increasing opacity (default)
+
 function paint() {
+
   const squares = document.querySelectorAll('.content');
-  
   squares.forEach((square) => {
 
     square.addEventListener('mouseover', (e) => {
+
       square.style.backgroundColor = 'black';
       let opacity = Number(square.style.opacity); 
       opacity += 0.1;
@@ -45,24 +48,34 @@ function paint() {
 
 }
 
-function paintRandom() {
-  const squares = document.querySelectorAll('.content');
+// Paints each square on hover with a random color
 
+function paintRandom() {
+
+  const squares = document.querySelectorAll('.content');
   squares.forEach((square) => {
 
     square.addEventListener('mouseover', (e) => {
+
       square.style.backgroundColor = random_bg_color();
+      square.style.opacity = 1;
     })
   })
 
 }
 
+// Modifies the css grid template to match the chosen grid size
+
 function gridTemplate(x) {
+
   container.style.gridTemplateRows = "repeat(" + x + ", 1fr)";
   container.style.gridTemplateColumns = "repeat(" + x + ", 1fr)";
 }
 
+// Generates random color
+
 function random_bg_color() {
+
   let x = Math.floor(Math.random() * 256);
   let y = Math.floor(Math.random() * 256);
   let z = Math.floor(Math.random() * 256);
@@ -72,4 +85,3 @@ function random_bg_color() {
 button.addEventListener('click', newGrid);
 
 btn.addEventListener('click', paintRandom);
-
